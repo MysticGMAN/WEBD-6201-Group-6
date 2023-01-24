5555c4a694f253ed3515a7405f9d1824e25d0a8f
Grayson <grayson.gmeat@gmail.com>
2023-01-23 19:37:57 -0500

    First Commit

---
+++ Assignment1-Group6/README.md
@@ -0,0 +1,3 @@
+Group 6 Assignment 1 WEBD6201
+
+Group 6 consists of Conner Tammane & Grayson Closs
---
+++ Assignment1-Group6/Scripts/app.js
@@ -0,0 +1,49 @@
+"use strict";
+
+
+//IIFE - Immediately Invoked Function Expression
+(function(){
+    function Start()
+    {
+        console.log("App Started")
+    }
+    window.addEventListener("load", Start)
+    if (document.title === "Home") { DisplayPage(); }
+})();
+
+//Function that listens for the click of the About Us button on the index.html page
+function DisplayPage(){
+
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p");
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the first javascript variable`;
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+    MainParagraph.textContent = SecondString;
+    MainContent.appendChild(MainParagraph);
+
+
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+    Article.setAttribute("class", "container");
+    Article.innerHTML = ArticleParagraph;
+    document.body.appendChild(Article);
+
+    let ContactUsBtn = document.getElementById("ContactUsBtn");
+    let ServicesBtn = document.getElementById("ServicesBtn");
+    let ProductsBtn = document.getElementById("ProductsBtn");
+    let AboutUsButton = document.getElementById("AboutUsBtn");
+    document.getElementById("AboutUsBtn").addEventListener("click", function(){
+        location.href = "about.html";
+    });
+    ProductsBtn.addEventListener("click", function(){
+        location.href = "products.html";
+    });
+    ServicesBtn.addEventListener("click", function(){
+        location.href = "services.html";
+    });
+    ContactUsBtn.addEventListener("click", function(){
+        location.href = "contact.html";
+    });
+}
