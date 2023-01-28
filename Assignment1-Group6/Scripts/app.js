b27f46d6639c6d58b708dbf10bb4cb25c37214f2
tamanec905 <connertamane@hotmail.com>
2023-01-27 20:15:20 -0500

    2

--- Assignment1-Group6/Scripts/app.js
+++ Assignment1-Group6/Scripts/app.js
@@ -55,8 +55,58 @@ function IndexPage (){
     document.getElementById("bodyTxt").innerText = bodyText;
 }
 
-function ProjectPage(){
+function ProjectsPage(){
+    let welcomeH3 = document.getElementById("projectsH3");
+    let projectWelMsg = "Welcome to Projects";
+    welcomeH3.innerText = projectWelMsg;
+    let projBody = document.getElementById("projMainPara");
+    let projMsg = "Here you can see some of the projects we're most proud of!";
+    projBody.innerText = projMsg;
+    let connerCol = document.getElementById("connerCol");
+    let graysonCol = document.getElementById("graysonCol");
 
+    let cMsg = "The first picture on the left is my Java assignment for WEBD Enterprise. The one below is my WEB Development Site using PHP. And finally, the third is my Net Development" +
+        "application using C#.";
+    let gMsg = "the first picture on your right is my cobol very cool. The next is my comrade calc, to calculate our comrades pay. And lastly is the first java program i wrote, but just one file.";
+
+    connerCol.innerText = cMsg;
+    graysonCol.innerText = gMsg;
+}
+
+function ServicesPage(){
+    let welServices = document.getElementById("welServices");
+    let paraServices = document.getElementById("servicesPara");
+
+    let welMsg = "Welcome to services!";
+    welServices.innerText = welMsg;
+
+    let servicesMsg = "We offer many services such as Web and Mobile Development as well as Custom Programming. We also provide " +
+        "full stack development. Please send us a message using our <a href=\"contact.html\">'Contact Us'</a> page to see if we can be of service.";
+    paraServices.innerHTML = servicesMsg;
+}
+
+function AboutPage(){
+    let welAbout = document.getElementById("welAbout");
+    let gAboot = document.getElementById("graysonColAbout");
+    let cAboot = document.getElementById("connerColAbout");
+
+
+
+    let aboutWelMsg = "Aboot Us!";
+    welAbout.innerText = aboutWelMsg;
+
+    let aboutMsgC = "Hi, I'm Conner. Currently a Computer Programming Student at Durham College. I've always been fascinated by programming and how it's used in so many of our everyday items. " +
+    "It's one of the things I've never lost passion for since I was a kid and growing up only piqued my interest more. " +
+    "I'm always interested in solving problems in a strategic way and am always keeping track of time to maintain progress and productivity. " +
+        "I enjoy working with teams and meeting new people but am also great at working individually.";
+    cAboot.innerText = aboutMsgC;
+
+
+    let aboutMsgG = "Hi, I'm Grayson. Ever since I was 14, I've been doing mechanic work and general labour. " +
+        "It was hard work, but it taught me the important value of hard work, and how I do not want to do this for the rest of my life. " +
+        "So, I took my hobby and love for all things tech and started making it into my future career. " +
+        "I started my journey into programming at Durham college and hope to finish my 2-year programming course soon. I may even do the 3-year analyst program.";
+    gAboot.innerText = aboutMsgG;
 }
 
 function SiteWide (){
